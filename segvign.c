cf60986bdcc7068c3df0836e068011a9d6ff3821
Simon Brenner <olsner@gmail.com>
2020-08-15 17:35:58 +0200

    Implement On Error Goto (with a funnier unsafe variant)

--- segvign.c
+++ segvign.c
@@ -1,5 +1,6 @@
 #define _GNU_SOURCE
 
+#include <assert.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -7,13 +8,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ucontext.h>
+
 #define REGISTERS_H // conflicts with ucontext
 #include <bddisasm.h>
 
 #define print_instruction 0
 typedef uint64_t u64;
 
+#define ERROR_RESUME_NEXT (void*)-1
+#define ERROR_SET_CONTEXT (void*)-2
+
+void* volatile error_label = ERROR_RESUME_NEXT;
+volatile sig_atomic_t error_signal = 0;
+ucontext_t error_context;
+
 void segv_action(int sig, siginfo_t* info, void* contextp) {
+    // Save error code for error handler when using error goto
+    error_signal = sig;
+
     printf("SIGSEGV received (w/ info and context)\n");
     ucontext_t* context = contextp;
     void** prip = (void**)&context->uc_mcontext.gregs[REG_RIP];
@@ -26,9 +38,21 @@ void segv_action(int sig, siginfo_t* info, void* contextp) {
 #if print_instruction
     char buf[ND_MIN_BUF_SIZE];
     NdToText(&ix, (u64)*prip, sizeof(buf), buf);
-    printf("Fault instruction: %s\n", buf);
+    printf("Fault instruction: [%p] %s\n", *prip, buf);
 #endif
-    *prip += ix.Length;
+    // TODO Save state for a future Resume Next function called from the error handler.
+    if (error_label == ERROR_RESUME_NEXT) {
+        *prip += ix.Length;
+        printf("RESUME_NEXT rip=%p\n", *prip);
+    } else if (error_label == ERROR_SET_CONTEXT) {
+        printf("SET_CONTEXT rip=%p\n", (void*)error_context.uc_mcontext.gregs[REG_RIP]);
+        setcontext(&error_context);
+    } else {
+        printf("GOTO LABEL %p\n", error_label);
+        // Yes, this just plops a new RIP in. Seems more funny than actually doing a longjmp kind of thing
+        // that has code in the target function. Probably a lot less safe though :)
+        *prip = error_label;
+    }
     setcontext(context);
     perror("setcontext");
 }
@@ -49,7 +73,8 @@ void* nd_memset(void *s, int c, size_t n)
     return memset(s, c, n);
 }
 
-int on_error_resume_next(void) {
+int set_signal_handler(void) {
+    printf("Setting signal handler...\n");
     struct sigaction sa;
     if (sigaction(SIGSEGV, NULL, &sa)) {
         perror("sigaction");
@@ -64,24 +89,93 @@ int on_error_resume_next(void) {
     return 0;
 }
 
-void segfault(int* flag) {
+int on_error_resume_next(void) {
+    error_label = ERROR_RESUME_NEXT;
+    return set_signal_handler();
+}
+
+#define on_error_goto(label) do { \
+    error_label = ERROR_SET_CONTEXT; \
+    error_signal = 0; \
+    set_signal_handler(); \
+    getcontext(&error_context); \
+    printf("on_error_getcontext returned with error_signal=%d\n", error_signal); \
+    if (error_signal) { \
+        printf("going to error label " #label "\n"); \
+        goto label; \
+    } \
+} while (0)
+
+#define on_error_goto_unsafe(label) do { \
+    error_signal = 0; error_label = &&label; \
+    /* Trick the compiler into keeping label reachable. */ \
+    if (error_signal) goto label; \
+    set_signal_handler(); \
+} while (0)
+
+static void segfault(volatile int* flag) {
     *flag = 1234;
     *(int*)0xdeadbeef = 0xcafebabe;
     *flag = 1;
 }
 
-int main() {
-    printf("Setting signal handler...\n");
-    on_error_resume_next();
+#define SEGFAULT(pflag) do { \
+    *(pflag) = 1234; \
+    *(int*)0xdeadbeef = 0xcafebabe; \
+    *(pflag) = 1; \
+} while (0)
+
+static bool test_segfault(void) {
     printf("Segfaulting...\n");
-    int flag = 0;
+    volatile int flag = 0;
     segfault(&flag);
     if (flag == 1) {
-        printf("Resumed successfully, exiting.\n");
-        return 0;
+        printf("Resumed after segfault (flag=1)\n");
+        return true;
     }
     else {
-        printf("Didn't resume correctly? flag is %d but should be 1\n", flag);
-        return 1;
+        printf("Not resumed after segfault (flag=%d)\n", flag);
+        return false;
     }
+}
+
+void test_resume_next(void) {
+    printf("Testing error_resume_next: should resume\n");
+    on_error_resume_next();
+    bool resumed = test_segfault();
+    assert(resumed);
+}
+
+void test_error_goto(void) {
+    printf("Testing error_goto: should not resume\n");
+    on_error_goto(error_handler);
+    test_segfault();
+    assert(false); // should not get here - should go to the error handler instead
+    return;
+
+error_handler:
+    printf("error_goto: At error handler as expected.\n");
+}
+
+void test_error_goto_unsafe(void) {
+    // Note: only safe for errors in the same stack frame as the function,
+    // otherwise it's as if the called function jumped to the label, then
+    // returns to its caller.
+    printf("Testing error_goto_unsafe: should not resume\n");
+    on_error_goto_unsafe(error_handler);
+    volatile int flag = 0;
+    SEGFAULT(&flag);
+    assert(false); // should not get here - should go to the error handler instead
+    return;
+
+error_handler:
+    printf("error_goto_unsafe: At error handler as expected.\n");
+    printf("flag = %d (expect 1234)\n", flag);
+}
+
+int main() {
+    test_resume_next();
+    test_error_goto();
+    test_error_goto_unsafe();
+    printf("\nTests completed successfully.\n");
 }
